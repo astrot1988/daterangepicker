244f75f53e76a02e924ed4d27362f7877e4f7320
astrot <astrot1988@gmail.com>
2020-10-05 16:44:29 +0300

    bck

--- daterangepicker.js
+++ daterangepicker.js
@@ -1048,7 +1048,7 @@
             var parentOffset = { top: 0, left: 0 },
                 containerTop;
             var parentRightEdge = $(window).width();
-            if (!this.parentEl === 'body') {
+            if (!this.parentEl.is('body')) {
                 parentOffset = {
                     top: this.parentEl.offset().top - this.parentEl.scrollTop(),
                     left: this.parentEl.offset().left - this.parentEl.scrollLeft()
